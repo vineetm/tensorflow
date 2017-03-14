03492767020b589bbc75fd346373a695c428534c
Vineet Kumar <vineeku6@in.ibm.com>
2017-03-14 12:32:40 +0530

    Don't use sampled softmax

--- tensorflow/models/rnn/translate/translate.py
+++ tensorflow/models/rnn/translate/translate.py
@@ -161,7 +161,8 @@ def create_model(session, forward_only):
       FLAGS.batch_size,
       FLAGS.learning_rate,
       FLAGS.learning_rate_decay_factor,
-      forward_only=forward_only)
+      forward_only=forward_only,
+      num_samples=-1)
 
   ckpt = tf.train.get_checkpoint_state(FLAGS.train_dir)
   if ckpt and tf.train.checkpoint_exists(ckpt.model_checkpoint_path):
