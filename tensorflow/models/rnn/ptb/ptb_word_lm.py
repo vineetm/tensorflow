e2cb90ffcdb088855cb59dc32b0063b79f6d6f16
Vineet Kumar <vineeku6@in.ibm.com>
2017-04-05 18:20:34 +0530

    Fix old references

--- tensorflow/models/rnn/ptb/ptb_word_lm.py
+++ tensorflow/models/rnn/ptb/ptb_word_lm.py
@@ -102,7 +102,7 @@ class PTBModel(object):
 
     lstm_cell = tf.contrib.rnn.BasicLSTMCell(size, forget_bias=0.0, state_is_tuple=True)
     if is_training and config.keep_prob < 1:
-      lstm_cell = tf.nn.rnn_cell.DropoutWrapper(
+      lstm_cell = tf.contrib.rnn.DropoutWrapper(
           lstm_cell, output_keep_prob=config.keep_prob)
     cell = tf.contrib.rnn.MultiRNNCell([lstm_cell] * config.num_layers, state_is_tuple=True)
 
